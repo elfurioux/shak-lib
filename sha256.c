5700ad7c2674e84c0d583b3f94374c0aae7c07a0
❊ elfurioux ❊ <60098198+elfurioux@users.noreply.github.com>
2024-02-23 00:40:30 +0100

    Done the padding & parsing step

--- sha256.c
+++ sha256.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 #define MSGMAXLEN 1024
 #define SHA256_CONST_LEN 64
 
 typedef unsigned long long word64;
 typedef unsigned int word32;
+typedef unsigned char uint8;
 typedef unsigned char word8;
 typedef struct BLOCK {
     word32 w0;  word32 w1;  word32 w2;  word32 w3;
@@ -15,7 +17,8 @@ typedef struct BLOCK {
     word32 w12; word32 w13; word32 w14; word32 w15;
 }BLOCK;
 
-const long int SHA256_CONST[SHA256_CONST_LEN] = {
+
+const word32 SHA256_CONST[SHA256_CONST_LEN] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -26,15 +29,22 @@ const long int SHA256_CONST[SHA256_CONST_LEN] = {
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc > 2) {
         fprintf(stderr, "USAGE: %s <message>", argv[0]);
         return EXIT_FAILURE;
+    } else if (argc == 1) {
+        argv[1] = "\0";
     }
+
     if (strlen(argv[1]) > MSGMAXLEN) {
         fprintf(stderr, "ERROR: <message> IS OVER %d BYTES. NOT SUPPORTED AT THE MOMENT.", MSGMAXLEN);
         return EXIT_FAILURE;
     }
+
+
+    /* ==================================== MESSAGE PADDING ==================================== */
 
     // lenghts are in bits
     int msglen = strlen(argv[1])*8;
@@ -50,6 +60,82 @@ int main(int argc, char *argv[]) {
     printf("l=%d=(%d*512+%d) k=%d\n",msglen,msglen/512,msglen%512,zbitcount);
     printf("l+1+k = %d = %dmod512\n",(msglen+1+zbitcount),(msglen+1+zbitcount)%512);
     printf("block_count=%d\n",block_count);
+    printf("================-================-================-================-================-================-================-================\n");
+
+    // puts the characters in the word8 message array
+    word8 message[MSGMAXLEN];
+    int i = 0;
+    while (zbitcount > 0) {
+        if (i < msglen/8) {
+            message[i] = argv[1][i];
+        } else if (i == msglen/8) {
+            zbitcount -= 7;
+            message[i] = 0b10000000;
+        } else {
+            zbitcount -= 8;
+            message[i] = 0b00000000;
+        }
+        i++;
+    }
+    // appends the message lenght to the end of the message blocks
+    int j = i; // offset
+    word64 msglen_o = msglen/8;
+    for (i = 0; i < 8; i++) {
+        message[j+i] = (word8)(msglen_o>>56);
+        msglen_o = msglen_o<<8;
+    }
+
+    // Prints the unparsed message blocks
+    for (int i = 0; i < block_count*64 ; i++) {
+        printf("%.2X",message[i]);
+        if (i%64 == 63) {
+            printf("\n");
+        } else if (i%8==7) {
+            printf(" ");
+        }
+    }
+    printf("================-================-================-================-================-================-================-================\n");
+
+
+    /* =================================== PARSE THE MESSAGE =================================== */
+
+    BLOCK mblocks[block_count];
+    int offset = 0;
+    for (int i = 0; i < block_count; i++) {
+        offset = (i*512)/8;
+
+        mblocks[i].w0    = (word32)(message[offset+0]<<24)  + (word32)(message[offset+1]<<16)  + (word32)(message[offset+2]<<8)  +(word32)(message[offset+3]);
+        mblocks[i].w1    = (word32)(message[offset+4]<<24)  + (word32)(message[offset+5]<<16)  + (word32)(message[offset+6]<<8)  +(word32)(message[offset+7]);
+        mblocks[i].w2    = (word32)(message[offset+8]<<24)  + (word32)(message[offset+9]<<16)  + (word32)(message[offset+10]<<8) +(word32)(message[offset+11]);
+        mblocks[i].w3    = (word32)(message[offset+12]<<24) + (word32)(message[offset+13]<<16) + (word32)(message[offset+14]<<8) +(word32)(message[offset+15]);
+        offset += 16;
+        mblocks[i].w4    = (word32)(message[offset+0]<<24)  + (word32)(message[offset+1]<<16)  + (word32)(message[offset+2]<<8)  +(word32)(message[offset+3]);
+        mblocks[i].w5    = (word32)(message[offset+4]<<24)  + (word32)(message[offset+5]<<16)  + (word32)(message[offset+6]<<8)  +(word32)(message[offset+7]);
+        mblocks[i].w6    = (word32)(message[offset+8]<<24)  + (word32)(message[offset+9]<<16)  + (word32)(message[offset+10]<<8) +(word32)(message[offset+11]);
+        mblocks[i].w7    = (word32)(message[offset+12]<<24) + (word32)(message[offset+13]<<16) + (word32)(message[offset+14]<<8) +(word32)(message[offset+15]);
+        offset += 16;
+        mblocks[i].w8    = (word32)(message[offset+0]<<24)  + (word32)(message[offset+1]<<16)  + (word32)(message[offset+2]<<8)  +(word32)(message[offset+3]);
+        mblocks[i].w9    = (word32)(message[offset+4]<<24)  + (word32)(message[offset+5]<<16)  + (word32)(message[offset+6]<<8)  +(word32)(message[offset+7]);
+        mblocks[i].w10   = (word32)(message[offset+8]<<24)  + (word32)(message[offset+9]<<16)  + (word32)(message[offset+10]<<8) +(word32)(message[offset+11]);
+        mblocks[i].w11   = (word32)(message[offset+12]<<24) + (word32)(message[offset+13]<<16) + (word32)(message[offset+14]<<8) +(word32)(message[offset+15]);
+        offset += 16;
+        mblocks[i].w12   = (word32)(message[offset+0]<<24)  + (word32)(message[offset+1]<<16)  + (word32)(message[offset+2]<<8)  +(word32)(message[offset+3]);
+        mblocks[i].w13   = (word32)(message[offset+4]<<24)  + (word32)(message[offset+5]<<16)  + (word32)(message[offset+6]<<8)  +(word32)(message[offset+7]);
+        mblocks[i].w14   = (word32)(message[offset+8]<<24)  + (word32)(message[offset+9]<<16)  + (word32)(message[offset+10]<<8) +(word32)(message[offset+11]);
+        mblocks[i].w15   = (word32)(message[offset+12]<<24) + (word32)(message[offset+13]<<16) + (word32)(message[offset+14]<<8) +(word32)(message[offset+15]);
+    }
+
+    // prints the message blocks, parsed this time
+    for (int i = 0; i < block_count; i++) {
+        printf("%.8X%.8X %.8X%.8X %.8X%.8X %.8X%.8X %.8X%.8X %.8X%.8X %.8X%.8X %.8X%.8X\n",
+            mblocks[i].w0,mblocks[i].w1,mblocks[i].w2,mblocks[i].w3,
+            mblocks[i].w4,mblocks[i].w5,mblocks[i].w6,mblocks[i].w7,
+            mblocks[i].w8,mblocks[i].w9,mblocks[i].w10,mblocks[i].w11,
+            mblocks[i].w12,mblocks[i].w13,mblocks[i].w14,mblocks[i].w15
+        );
+    }
+    printf("================-================-================-================-================-================-================-================\n");
+    printf("done!");
 
     return EXIT_SUCCESS;
 }
